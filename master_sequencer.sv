142217197c48a6675e13cc39b45abe634cf6b6ee
samarth_patel <67362744+samarth-py@users.noreply.github.com>
2023-02-12 01:28:00 +0530

    Create master_sequencer.sv

---
+++ master_sequencer.sv
@@ -0,0 +1,8 @@
+class master_sequencer extends uvm_sequencer #(axi_xtn);
+`uvm_component_utils(master_sequencer)
+extern function new(string name="master_sequencer",uvm_component parent);
+endclass
+
+function master_sequencer::new(string name="master_sequencer",uvm_component parent);
+super.new(name,parent);
+endfunction
